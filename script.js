7998b9d1bb7ff64f02b7b58ed42f0cec30e3b97d
Pedro Elias <145694371+Elias-Pedro@users.noreply.github.com>
2023-11-28 15:50:51 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 let titleName = document.querySelectorAll(".titleName")
 for (let i = 0; i < titleName.length; i++) {
-    titleName[i].innerHTML = "Angel Tattoo"
+    titleName[i].innerHTML = "Ruan Fellipe"
 }
 let endereco = document.querySelector(".endereco")
-endereco.innerHTML = "Rua Coronel Carvalho, 09 - Centro, Angra dos Reis - RJ, 23900-310"
+endereco.innerHTML = "R. Quaresma Júnior, 160 - Sala 505 - Centro, Angra dos Reis - RJ, 23900-282"
 
 let enderecoMapa = document.querySelector(".enderecoMapa")
 //enderecoMapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d64440159.17531167!2d-99.76709813773608!3d-9.671198649557327!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9c59c7ebcc28cf%3A0x295a1506f2293e63!2sBrasil!5e0!3m2!1spt-BR!2sbr!4v1701131771472!5m2!1spt-BR!2sbr"
-enderecoMapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.488813176697!2d-44.318233525302496!3d-23.00581784141665!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9c4f6ea466fc2d%3A0x48ebf684775df4c1!2sAngel%20Tattoo!5e0!3m2!1spt-BR!2sbr!4v1701192336570!5m2!1spt-BR!2sbr"
+enderecoMapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.4086447684695!2d-44.321641424686526!3d-23.008763379183378!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9c4fb3f1024a29%3A0x1853bc99edcfd0cd!2sBorel%20Tattoo%20Studio!5e0!3m2!1spt-BR!2sbr!4v1701196680142!5m2!1spt-BR!2sbr"
 
 
 
