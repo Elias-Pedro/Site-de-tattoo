683ff40215ffd773c20561b41f5d2d06937e7767
Pedro Elias <145694371+Elias-Pedro@users.noreply.github.com>
2023-11-28 12:09:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+let titleName = document.querySelectorAll(".titleName")
+for (let i = 0; i < titleName.length; i++) {
+    titleName[i].innerHTML = "Joanna Rodrigues"
+}
+let endereco = document.querySelector(".endereco")
+endereco.innerHTML = "???"
+
+let enderecoMapa = document.querySelector(".enderecoMapa")
+enderecoMapa.src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d64440159.17531167!2d-99.76709813773608!3d-9.671198649557327!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9c59c7ebcc28cf%3A0x295a1506f2293e63!2sBrasil!5e0!3m2!1spt-BR!2sbr!4v1701131771472!5m2!1spt-BR!2sbr"
+
+
+
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+var pageUp = document.querySelector(".pageUp");
+var scrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+if (scrollPosition > 0.8 * window.innerHeight) {
+    pageUp.style.display = "flex";
+} else {
+    pageUp.style.display = "none";
+}
+}
+
+function whatsApp(){
+    //window.open('')
+}
+function faceBook(){
+    //window.open('')
+}
+function instagram(){
+    //window.open('')
+}
+function tikTok(){
+    //window.open('')
+}
+
+let shadow = document.querySelector(".shadow")
+function addComentario(){
+    shadow.style.display = "flex"
+}
+function cancelarComentario(){
+    shadow.style.display = "none"
+
+}
+
+
+let btnAbrirMenu = document.querySelector(".btnAbrirMenu")
+let btnFecharMenu = document.querySelector(".btnFecharMenu")
+let logoHeader = document.querySelector(".logoHeader")
+let itemsMenu = document.querySelector(".itemsMenu")
+
+btnFecharMenu.style.display = "none"
+
+function abrirMenu(){
+    btnAbrirMenu.style.display = "none"
+    btnFecharMenu.style.display = "flex"
+    logoHeader.style.display = "none"
+    itemsMenu.style.display = "flex"
+}
+function fecharMenu(){
+    btnFecharMenu.style.display = "none"
+    btnAbrirMenu.style.display = "flex"
+    logoHeader.style.display = "flex"
+    itemsMenu.style.display = "none"
+
+}
